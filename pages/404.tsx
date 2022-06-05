0fc7c7785f3125fcb5238d25cdc8cab144c64aac
Xaron11 <kaczmarzykdawid4@gmail.com>
2022-06-05 14:33:04 +0200

    Custom error pages

---
+++ pages/404.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Link from 'next/link';
+import { createStyles, Title, Text, Button, Container, Group } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: 80,
+    paddingBottom: 80,
+  },
+
+  label: {
+    textAlign: 'center',
+    fontWeight: 900,
+    fontSize: 220,
+    lineHeight: 1,
+    marginBottom: theme.spacing.xl * 1.5,
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2],
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 120,
+    },
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    textAlign: 'center',
+    fontWeight: 900,
+    fontSize: 38,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 32,
+    },
+  },
+
+  description: {
+    maxWidth: 500,
+    margin: 'auto',
+    marginTop: theme.spacing.xl,
+    marginBottom: theme.spacing.xl * 1.5,
+  },
+}));
+
+export default function NotFound() {
+  const { classes } = useStyles();
+
+  return (
+    <Container className={classes.root}>
+      <div className={classes.label}>404</div>
+      <Title className={classes.title}>Tutaj niczego nie ma</Title>
+      <Text color="dimmed" size="lg" align="center" className={classes.description}>
+        Strona, którą próbujesz otworzyć, nie istnieje. Możliwe, że błędnie wpisałeś adres lub strona została
+        przeniesiona pod inny adres URL. Jeśli uważasz, że jest to błąd, skontaktuj się z działem pomocy technicznej.
+      </Text>
+      <Group position="center">
+        <Link href="/">
+          <Button variant="subtle" size="md">
+            Przejdź do strony głównej
+          </Button>
+        </Link>
+      </Group>
+    </Container>
+  );
+}
---
+++ pages/500.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { createStyles, Title, Text, Button, Container, Group } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: 80,
+    paddingBottom: 120,
+    backgroundColor: theme.colors[theme.primaryColor][6],
+  },
+
+  label: {
+    textAlign: 'center',
+    fontWeight: 900,
+    fontSize: 220,
+    lineHeight: 1,
+    marginBottom: theme.spacing.xl * 1.5,
+    color: theme.colors[theme.primaryColor][3],
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 120,
+    },
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    textAlign: 'center',
+    fontWeight: 900,
+    fontSize: 38,
+    color: theme.white,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: 32,
+    },
+  },
+
+  description: {
+    maxWidth: 540,
+    margin: 'auto',
+    marginTop: theme.spacing.xl,
+    marginBottom: theme.spacing.xl * 1.5,
+    color: theme.colors[theme.primaryColor][1],
+  },
+}));
+
+export default function ServerError() {
+  const { classes } = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Container>
+        <div className={classes.label}>500</div>
+        <Title className={classes.title}>Coś poszło nie tak...</Title>
+        <Text size="lg" align="center" className={classes.description}>
+          Nasze serwery nie mogły obsłużyć Twojego żądania. Spróbuj odświeżyć stronę.
+        </Text>
+        <Group position="center">
+          <Button variant="white" size="md">
+            Odśwież stronę
+          </Button>
+        </Group>
+      </Container>
+    </div>
+  );
+}
