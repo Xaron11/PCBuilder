1789f572b1fcc30898d4b7632261c4d576c3abdf
Xaron11 <kaczmarzykdawid4@gmail.com>
2022-06-14 19:30:49 +0200

    Temporary API response error logging #2

--- pages/parts/[part].tsx
+++ pages/parts/[part].tsx
@@ -121,12 +121,12 @@ export async function getStaticProps(context: GetStaticProps & { params: { part:
   const { part } = context['params'];
   const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL ?? '';
   const res = await fetch(apiUrl + `/parts/${part}`);
-  console.log(res.body);
+  console.log(res.text());
   let data;
   try {
     data = await res.json();
   } catch (error) {
-    throw new Error(`${error}\n${res.status}\n${res.body}`);
+    throw new Error(`${error}\n${res.status}\n${res.text()}`);
   }
   console.log(data);
   const items: PartItem[] = data;
