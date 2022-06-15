de6b883508db2de01c0d62e6e4b3453e0896013c
Xaron11 <kaczmarzykdawid4@gmail.com>
2022-06-15 10:37:33 +0200

    Static PartSlot property

--- components/partSlot.tsx
+++ components/partSlot.tsx
@@ -8,7 +8,12 @@ import { PartItem } from '../types/parts';
 import { Trash, Plus } from 'tabler-icons-react';
 import { PartInformation } from './partCard';
 
-export default function PartSlot(props: { partName: string; partId: string; partItem: PartItem | null }) {
+export default function PartSlot(props: {
+  static?: boolean;
+  partName: string;
+  partId: string;
+  partItem: PartItem | null;
+}) {
   const theme = useMantineTheme();
   const xsBreakpoint = useMediaQuery(`(max-width: ${theme.breakpoints.xs}px)`);
   const { parts, setParts } = usePartsContext() as PartsContextType;
@@ -42,23 +47,27 @@ export default function PartSlot(props: { partName: string; partId: string; part
         </Group>
       ) : xsBreakpoint ? (
         <Stack p="sm" style={{ position: 'relative' }}>
-          <Center>
-            <Image
-              src={`https://via.placeholder.com/300x200.jpeg?text=${props.partId}`}
-              alt="Zdjęcie poglądowe"
-              width={300}
-              height={200}
-            />
-          </Center>
-          <ActionIcon
-            style={{ position: 'absolute', right: 10 }}
-            size="lg"
-            variant="filled"
-            color="red"
-            onClick={handleRemovePart}
-          >
-            <Trash />
-          </ActionIcon>
+          {props.static != true && (
+            <>
+              <Center>
+                <Image
+                  src={`https://via.placeholder.com/300x200.jpeg?text=${props.partId}`}
+                  alt="Zdjęcie poglądowe"
+                  width={300}
+                  height={200}
+                />
+              </Center>
+              <ActionIcon
+                style={{ position: 'absolute', right: 10 }}
+                size="lg"
+                variant="filled"
+                color="red"
+                onClick={handleRemovePart}
+              >
+                <Trash />
+              </ActionIcon>
+            </>
+          )}
           <Stack style={{ flex: 1 }}>
             <Group position="apart" noWrap align="flex-start">
               <Text style={{ fontSize: 24 }} weight={500}>
@@ -74,33 +83,35 @@ export default function PartSlot(props: { partName: string; partId: string; part
         </Stack>
       ) : (
         <Group p="sm" align="flex-start">
-          <Stack justify="center">
-            <Button
-              leftIcon={<Trash />}
-              radius="md"
-              color="red"
-              styles={(theme) => ({
-                root: {
-                  minWidth: 250,
-                  width: `40%`,
-                },
-                inner: {
-                  justifyContent: 'flex-start',
-                },
-              })}
-              onClick={handleRemovePart}
-            >
-              <Text size="lg">{props.partName}</Text>
-            </Button>
-            <Center>
-              <Image
-                src={`https://via.placeholder.com/250x150.jpeg?text=${props.partId}`}
-                width={250}
-                height={150}
-                alt="Zdjęcie poglądowe"
-              />
-            </Center>
-          </Stack>
+          {props.static != true && (
+            <Stack justify="center">
+              <Button
+                leftIcon={<Trash />}
+                radius="md"
+                color="red"
+                styles={(theme) => ({
+                  root: {
+                    minWidth: 250,
+                    width: `40%`,
+                  },
+                  inner: {
+                    justifyContent: 'flex-start',
+                  },
+                })}
+                onClick={handleRemovePart}
+              >
+                <Text size="lg">{props.partName}</Text>
+              </Button>
+              <Center>
+                <Image
+                  src={`https://via.placeholder.com/250x150.jpeg?text=${props.partId}`}
+                  width={250}
+                  height={150}
+                  alt="Zdjęcie poglądowe"
+                />
+              </Center>
+            </Stack>
+          )}
           <Stack style={{ flex: 1 }}>
             <Text align="center" style={{ fontSize: 24 }} weight={500}>
               {props.partItem.brand}{' '}
